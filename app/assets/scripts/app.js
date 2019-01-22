dd66236d3a9acc56c612698e53b8ac09d33fd16d
Patrick <patrick@patricklensen.nl>
2019-01-22 22:53:27 +0100

    Ädded JS to toggle menu on mobile

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,17 +1,3 @@
-var $ = require('jquery');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-	payTaxes() {
-		console.log(this.name + ' now owes $0 in taxes.')
-	}
-}
-
-var john = new Person("John Doe", 'Blue');
-john.greet();
-
-var jane = new Adult("Jane Smith", "Purple");
-jane.greet();
-jane.payTaxes();
-
-console.log('Still working');
+var mobileMenu = new MobileMenu();
